05289c6b586183e16cdaa71e631660e919d546ab
Y_Wang <40142450+alexwang91@users.noreply.github.com>
2026-06-24 19:21:30 +0200

    Export evidence store

--- src/geo_agent/__init__.py
+++ src/geo_agent/__init__.py
@@ -6,6 +6,7 @@ from .entity_profile import (
     ValidationIssue,
     validate_entity_profile,
 )
+from .evidence_store import EvidenceStore
 from .query_space import INTENT_TYPES, QueryRecord, build_query_space
 
 __all__ = [
@@ -13,6 +14,7 @@ __all__ = [
     "EntityProfileValidationError",
     "ValidationIssue",
     "validate_entity_profile",
+    "EvidenceStore",
     "INTENT_TYPES",
     "QueryRecord",
     "build_query_space",
