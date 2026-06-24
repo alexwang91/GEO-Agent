49cf04b26e020dd81b43afb1568c46b8107d852d
Y_Wang <40142450+alexwang91@users.noreply.github.com>
2026-06-24 18:05:39 +0200

    Export M2 query space API

--- src/geo_agent/__init__.py
+++ src/geo_agent/__init__.py
@@ -6,10 +6,14 @@ from .entity_profile import (
     ValidationIssue,
     validate_entity_profile,
 )
+from .query_space import INTENT_TYPES, QueryRecord, build_query_space
 
 __all__ = [
     "EntityProfile",
     "EntityProfileValidationError",
     "ValidationIssue",
     "validate_entity_profile",
+    "INTENT_TYPES",
+    "QueryRecord",
+    "build_query_space",
 ]
